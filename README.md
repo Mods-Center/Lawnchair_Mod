2de2f6a84066214d79beba2b8ad5f0da664c2019
anant-goel <100245626+anant-goel@users.noreply.github.com>
2023-02-10 03:19:27 +0530

    initial commit
    ## Changelogs ##
    • Added LawnExtension to control various preferences
    • Themed icons package can be switched generally
    • Working for both Miui and Aosp as well (Choose while installing)
    • Only for A11/A12
    
    Co-Authored-By: Mods-Center <119596471+Mods-Center@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,37 @@
+# Modded Lawnchair for MIUI/AOSP
+ 
+![Picsart_22-12-14_07-47-34-971](https://blankary.com/image/77a95b7f)
+
+Lawnchair for MIUI/AOSP modified by [Kashi](https://t.me/kakashi1v1) with many features unlocked.
+Its installation is via Magisk on MIUI 12-13 based ROMs with Android 11/12.
+
+**Changelog:**
+• Added LawnExtension to control various preferences 
+• Themed icons package can be switched generally 
+• Working for both Miui and Aosp as well (Choose while installing)
+• Only for A11/A12
+
+For issues or questions you can send a message in [Telegram](https://t.me/bootloop_discussion) group.
+
+
+### Download
+
+Download from [releases page](https://github.com/Mods-Center/Lawnchair_Mod/releases)
+
+
+**Instructions**
+• Download and flash the zip in magisk
+• Reboot the device
+• Now Enjoy
+
+### Screenshots
+![photo_2022-12-16_21-10-41](https://t.me/cringe_screenshots/102)
+![photo_2022-12-16_21-10-37](https://t.me/cringe_screenshots/109)
+![photo_2022-12-16_21-10-31](https://t.me/cringe_screenshots/111)
+![photo_2022-12-16_21-10-53](https://t.me/cringe_screenshots/113)
+![photo_2022-12-16_21-10-49](https://t.me/cringe_screenshots/114)
+![photo_2022-12-16_21-10-45](https://t.me/cringe_screenshots/112)
+
+
+
+
---
+++ customize.sh
@@ -0,0 +1,82 @@
+REPLACE="
+/system/priv-app/MILauncher
+/system/priv-app/MiLauncher
+/system/priv-app/MILauncherGlobal
+/system/priv-app/MiLauncherGlobal
+/system/priv-app/MiuiHome
+/system/priv-app/MIUIHome
+/system/priv-app/MiuiLauncherGlobal
+/system/priv-app/MIUILauncherGlobal
+/system/product/priv-app/MILauncher
+/system/product/priv-app/MiLauncher
+/system/product/priv-app/MILauncherGlobal
+/system/product/priv-app/MiLauncherGlobal
+/system/product/priv-app/MiuiHome
+/system/product/priv-app/MIUIHome
+/system/product/priv-app/MiuiLauncherGlobal
+/system/product/priv-app/MIUILauncherGlobal
+"
+
+SKIPUNZIP=1
+SKIPMOUNT=false
+
+install_files() {
+    . $MODPATH/addon/install.sh
+
+ui_print " "
+ui_print " Warning: A13 is not supported."
+ui_print " "
+ui_print " "
+ui_print "Let's start"
+ui_print "Choose your Rom Version:"
+ui_print "  Vol+ = Miui"
+ui_print "  Vol- = Aosp"
+ui_print " "
+
+if chooseport; then
+    ui_print "- Miui selected"
+    cp -rf $MODPATH/files/launchermiui/MiuiHome.apk $MODPATH/system/priv-app/aMiuiHome
+    cp -rf $MODPATH/files/miuioverlay/QuickSwitchOverlay.apk $MODPATH/system/product/overlay
+else
+{
+    ui_print "- Aosp selected"
+    cp -rf $MODPATH/files/launcheraosp/Lawnchair.apk $MODPATH/system/priv-app/Lawnchair
+    cp -rf $MODPATH/files/aospoverlay/QuickSwitchOverlay.apk $MODPATH/system/product/overlay
+}
+fi
+
+}
+
+cleanup() {
+	rm -rf $MODPATH/addon 2>/dev/null
+	rm -rf $MODPATH/files 2>/dev/null
+	rm -f $MODPATH/install.sh 2>/dev/null
+	ui_print "- Deleting package cache files"
+    rm -rf /data/resource-cache/*
+    rm -rf /data/system/package_cache/*
+    rm -rf /cache/*
+    rm -rf /data/dalvik-cache/*
+    ui_print "- Launcher updates will be uninstalled..."
+    pm uninstall-system-updates com.miui.home
+    pm uninstall-system-updates app.lawnchair
+    ui_print "- Deleting old module (if it is installed)"
+    touch /data/adb/modules/lawnchair_launcher_mod/remove
+}
+
+run_install() {
+	ui_print " "
+	unzip -o "$ZIPFILE" -x 'META-INF/*' -d $MODPATH >&2
+	ui_print " "
+	ui_print "- Installing files"
+	install_files
+	sleep 1
+	ui_print " "
+	ui_print "- Cleaning up"
+	ui_print " "
+	cleanup
+	sleep 1
+	ui_print " "
+	ui_print "- Removing any Launcher folder to avoid issues"
+}
+
+run_install
